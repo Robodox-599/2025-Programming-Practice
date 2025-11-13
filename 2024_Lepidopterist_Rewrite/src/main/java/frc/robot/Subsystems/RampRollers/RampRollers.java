f92baa180780fb573b2e4bcda28bd0700e893719
Andre S <altsalehi1122@gmail.com>
2025-11-13 15:34:56 -0800

    worked on state machine logic

--- 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/Subsystems/RampRollers/RampRollers.java
+++ 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/Subsystems/RampRollers/RampRollers.java
@@ -8,7 +8,22 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class RampRollers {
   private final RampRollersIO io;
+  private WantedState wantedState = WantedState.STOPPED;
+  private CurrentState currentState = CurrentState.STOPPED;
 
+  public enum WantedState{
+    INTAKING,
+    HOLD_CORAL,
+    SCORING,
+    STOPPED,
+  }
+
+  public enum CurrentState{
+    INTAKING,
+    HOLD_CORAL,
+    SCORING,
+    STOPPED,
+  }
 
   /** Creates a new RampRollers. */
   public RampRollers(RampRollersIO io) {
@@ -16,7 +31,89 @@ public class RampRollers {
   }
 
   public void updateInputs() {
-    io.setPosition(0);
+    handleStateTransitions();
+    applyStates();
+  }
+
+  public void handleStateTransitions(){
+    switch(wantedState){
+      case STOPPED:
+        currentState = CurrentState.STOPPED;
+        break;
+      case INTAKING:
+        if(isCoralDetected()){
+          wantedState = WantedState.HOLD_CORAL;
+          currentState = CurrentState.HOLD_CORAL;
+        }
+        else{
+          currentState = CurrentState.INTAKING;
+        }
+        break;
+      case HOLD_CORAL:
+        if(!isCoralDetected()){
+          wantedState = WantedState.INTAKING;
+          currentState = CurrentState.INTAKING;
+        }
+        else{
+          currentState = CurrentState.HOLD_CORAL;
+        }
+      case SCORING:
+        if(!isCoralDetected()){
+          wantedState = WantedState.INTAKING;
+          currentState = CurrentState.INTAKING;
+        }
+        else{
+          currentState = CurrentState.SCORING;
+        }
+        break;
+      default:
+        currentState = CurrentState.STOPPED;
+        break;
+    }
+  }
+
+  public void applyStates(){
+    switch(currentState){
+      case STOPPED:
+        stop();
+        break;
+      case INTAKING:
+        setVelocity(0.5);
+        break;
+      case SCORING:
+        setVelocity(2);
+        break;
+      case HOLD_CORAL:
+        setPosition(currentPosition());
+        break;
+      default:
+        setVelocity(0);
+        break;
+    }
+  }
+
+  public void setVelocity(double velocity){
+    io.setVelocity(velocity);
+  }
+
+  public void stop(){
+    io.stop();
+  }
+
+  public void setWantedState(WantedState wantedState){
+    this.wantedState = wantedState;
+  }
+
+  public boolean isCoralDetected(){
+    return io.isCoralDetected;
+  }
+
+  public double currentPosition(){
+    return io.position;
+  }
+
+  public void setPosition(double position){
+    io.setPosition(position);
   }
 
 }
--- 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/Subsystems/RampRollers/RampRollersIO.java
+++ 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/Subsystems/RampRollers/RampRollersIO.java
@@ -5,12 +5,15 @@ public abstract class RampRollersIO {
     protected double velocity = 0.0;
 
     protected boolean isCoralDetected = false;
+    //protected double currentPosition = 0.0;
 
     public void updateInputs() {}
 
     public void setVelocity(double velocity) {}
 
     public void setPosition(double position) {}
+
+    //public double getPosition(){}
 
     public void stop() {}
 }
