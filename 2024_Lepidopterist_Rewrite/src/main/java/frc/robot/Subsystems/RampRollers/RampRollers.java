c1c3ca3fa822909033becad3346fe9c504e49b8c
Andre S <altsalehi1122@gmail.com>
2025-11-24 16:28:50 -0800

    fix speed again

--- 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/Subsystems/RampRollers/RampRollers.java
+++ 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/Subsystems/RampRollers/RampRollers.java
@@ -83,7 +83,7 @@ public class RampRollers {
         setVelocity(0.5);
         break;
       case SCORING:
-        setVelocity(-2);
+        setVelocity(-.3);
         break;
       case HOLD_CORAL:
         setPosition(io.heldCurrentPosition);
