63b6c7f3f4b105efcaeff15b2ba0fb160e44a30d
Andre S <altsalehi1122@gmail.com>
2025-12-08 15:30:46 -0800

    added kS for ramp rollers

--- 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/Subsystems/rollers/RampRollers/RampRollersConstants.java
+++ 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/Subsystems/rollers/RampRollers/RampRollersConstants.java
@@ -10,7 +10,7 @@ public class RampRollersConstants {
     public static final double kP = 0.45;
     public static final double kI = 0;
     public static final double kD = 0;
-    public static final double kS = 0;
+    public static final double kS = 0.03;
     public static final double kV = 0.124 * rampRollersGearRatio;
 
     public static final double supplyCurrentLimit = 40;
