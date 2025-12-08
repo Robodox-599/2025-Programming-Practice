0f52085ed016d212f77212e75edbc9226b207eb1
Andre S <altsalehi1122@gmail.com>
2025-12-08 15:32:50 -0800

    updated all pid constants for endeffector rollers

--- 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/Subsystems/rollers/endeffectorrollers/EndeffectorRollersConstants.java
+++ 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/Subsystems/rollers/endeffectorrollers/EndeffectorRollersConstants.java
@@ -9,12 +9,12 @@ public class EndeffectorRollersConstants {
     public static final String endeffectorRollersCANBus = "rio";
     public static final int beamBreakPort = 1;
     public static final double endeffectorRollersDebounceTimeSeconds = 0.2;
-    public static final double endeffectorRollersGearRatio = 2;
+    public static final double endeffectorRollersGearRatio = 3;
 
-    public static final double kP = 0.3;
+    public static final double kP = 0.45;
     public static final double kI = 0;
     public static final double kD = 0;
-    public static final double kS = 0;
+    public static final double kS = 0.03;
     public static final double kV = 0.124 * endeffectorRollersGearRatio;
 
     public static final double supplyCurrentLimit = 40;
