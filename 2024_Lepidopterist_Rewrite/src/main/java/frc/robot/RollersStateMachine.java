f619e79f53b95154415a27cbc9cc26f7b29e471c
Andre Salehi <altsalehi1122@gmail.com>
2025-12-07 17:55:19 -0800

    started adding rollers state machine

---
+++ 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/RollersStateMachine.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Subsystems.RampRollers.RampRollersIO;
+import frc.robot.Subsystems.endeffector.endeffectorrollers.EndeffectorRollersIO;
+
+public class RollersStateMachine extends SubsystemBase {
+  
+    private final EndeffectorRollersIO endIO;
+    private final RampRollersIO rampIO;
+
+  private WantedState wantedState = WantedState.STOPPED;
+  private CurrentState currentState = CurrentState.STOPPED;
+
+  public enum WantedState{
+    RAMP_INTAKING,
+    RAMP_HOLD_CORAL,
+    ROLLERS_INTAKE,
+    ENDEFFECTOR_HOLD_CORAL,
+    ENDEFFECTOR_SCORE,
+    STOPPED,
+  }
+
+  public enum CurrentState{
+    RAMP_INTAKING,
+    RAMP_HOLD_CORAL,
+    ROLLERS_INTAKE,
+    ENDEFFECTOR_HOLD_CORAL,
+    ENDEFFECTOR_SCORE,
+    STOPPED,
+  }
+
+  public RollersStateMachine(EndeffectorRollersIO endIO, RampRollersIO rampIO) {
+    this.endIO = endIO;
+    this.rampIO = rampIO;
+  }
+
+  public void setWantedState(WantedState wantedState){
+    this.wantedState = wantedState;
+  }
+}
