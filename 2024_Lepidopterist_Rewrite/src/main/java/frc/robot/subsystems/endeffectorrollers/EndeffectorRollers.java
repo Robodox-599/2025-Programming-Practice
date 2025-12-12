2c9805459000cc176476398a35f64a007055aa9f
Andre S <altsalehi1122@gmail.com>
2025-12-11 16:11:37 -0800

    fixed hold coral apply states logic

--- 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/subsystems/endeffectorrollers/EndeffectorRollers.java
+++ 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/subsystems/endeffectorrollers/EndeffectorRollers.java
@@ -87,7 +87,7 @@ public class EndeffectorRollers extends SubsystemBase {
         setVelocity(-0.5);
         break;
       case HOLD_CORAL:
-        stop();
+        setPosition(io.heldCurrentPosition);
         break;
       case SCORING_ALGAE:
         setVelocity(-0.8);
