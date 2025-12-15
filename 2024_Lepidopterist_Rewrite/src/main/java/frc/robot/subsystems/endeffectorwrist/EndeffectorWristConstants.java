4d09cafff15d77adc18ed963145c3fb515a01332
Andre S <altsalehi1122@gmail.com>
2025-12-15 15:50:35 -0800

    tuned stuff

--- 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/subsystems/endeffectorwrist/EndeffectorWristConstants.java
+++ 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/subsystems/endeffectorwrist/EndeffectorWristConstants.java
@@ -8,7 +8,7 @@ public class EndeffectorWristConstants {
 
     public static final double kP = 45;
     public static final double kI = 0;
-    public static final double kD = 6;
+    public static final double kD = 5;
     public static final double kS = 0;
     public static final double kV = 0.085;
     public static final double kG = 0.415;
@@ -17,6 +17,7 @@ public class EndeffectorWristConstants {
     public static final double cancoderMagnetOffset = -0.13720703125;
     public static final double discontinuityPoint = 0.0;
     public static final double isWristInPositionTolerence = 0.2;
-    public static final double MaxVelocity = 0.0;
-    public static final double MaxAcceleration = 0.0;
+
+    public static final double MaxVelocity = (12-kG-kS)/kV;
+    public static final double MaxAcceleration = (MaxVelocity/0.5);
 }
