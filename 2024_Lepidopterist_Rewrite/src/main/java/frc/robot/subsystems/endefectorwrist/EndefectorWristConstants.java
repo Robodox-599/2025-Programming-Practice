95881cfd0dd7e2b66098662a8a500248be3b0d4c
litlizlovesprog <lizzzz87658765@gmail.com>
2025-12-15 14:56:42 -0800

    constants

--- 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/subsystems/endefectorwrist/EndefectorWristConstants.java
+++ 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/subsystems/endefectorwrist/EndefectorWristConstants.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems.endefectorwrist;
 
 public class EndefectorWristConstants {
-    public static final int endefectorWristMotorID = 0;
+    public static final int endefectorWristMotorID = 15;
     public static final String endefectorWristCANBus = "rio";
-    public static final int cancoderID = 0;
+    public static final int cancoderID = 17;
     public static final double gearRatio = 0;
 
     public static final double kP = 0.3;
@@ -13,7 +13,7 @@ public class EndefectorWristConstants {
     public static final double kG = 0;
 
     public static final double supplyCurrentLimit = 40;
-    public static final double cancoderMagnetOffset = 0;
+    public static final double cancoderMagnetOffset = -0.13720703125;
     public static final double discontinuityPoint = 0;
     public static final double wristTolerance = 0;
 }
