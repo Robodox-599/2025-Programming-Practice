07d457350209ffa80a9eae2f1b15341ec2c8b917
litlizlovesprog <lizzzz87658765@gmail.com>
2025-12-14 19:40:45 -0800

    Finishing EndefectorWrist

--- 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/subsystems/endefectorwrist/EndefectorWrist.java
+++ 2024_Lepidopterist_Rewrite/src/main/java/frc/robot/subsystems/endefectorwrist/EndefectorWrist.java
@@ -1,5 +1,102 @@
 package frc.robot.subsystems.endefectorwrist;
 
+import dev.doglog.DogLog;
+
 public class EndefectorWrist {
-    
+ private final EndefectorWristIO io;
+ private WantedState wantedState = WantedState.STOPPED;
+ private CurrentState currentState = CurrentState.STOPPED;
+ 
+
+ public enum WantedState {
+  STOPPED,
+  INTAKING_CORAL,
+  INTAKING_ALGAE,
+  SCORE_CORAL,
+  SCORE_ALGAE,
+ }
+
+ public enum CurrentState {
+  STOPPED,
+  INTAKING_CORAL,
+  INTAKING_ALGAE,
+  SCORE_CORAL,
+  SCORE_ALGAE,
+ }
+
+  public EndefectorWrist(EndefectorWristIO io) {
+    this.io = io;
+  }
+
+  private void handleStateTransitions(){
+    switch (wantedState) {
+    case STOPPED:
+    currentState = CurrentState.STOPPED;
+    break;
+    case INTAKING_CORAL:
+        currentState = CurrentState.INTAKING_CORAL;
+    break;
+    case SCORE_CORAL:
+      currentState = CurrentState.SCORE_CORAL;
+    break;
+    case INTAKING_ALGAE:
+    currentState = CurrentState.INTAKING_ALGAE;
+    break;
+    case SCORE_ALGAE:
+    currentState = CurrentState.SCORE_ALGAE;
+    break;
+    default:
+    currentState = CurrentState.STOPPED;
+    break;}
+    }
+
+    private void applyState(){
+      switch (currentState) {
+      case STOPPED:
+        stop();
+        break;
+      case INTAKING_CORAL:
+        setPosition(0);
+        break;
+        case SCORE_CORAL:
+        setPosition(0);
+        break;
+      case INTAKING_ALGAE:
+        setPosition(0);
+        break;
+      case SCORE_ALGAE:
+         setPosition(0);
+         break;
+      default:
+        stop();
+        break;}
+      }
+
+      public void updateInputs(){
+        io.updateInputs();
+        handleStateTransitions();
+        applyState();
+        DogLog.log("EndefectorWrist/WantedState", wantedState.toString());
+        DogLog.log("EndefectorWrist/CurrentState", currentState.toString());
+      }
+
+      public void setVelocity(double velocity) {
+        io.setVelocity(velocity);
+      }
+
+      public void stop() {
+        io.stop();
+      }
+
+      public void setPosition(double position) {
+        io.setPosition(position);
+      }
+
+      public void setWantedState(WantedState wantedState) {
+        this.wantedState = wantedState;
+      }
+
+      public boolean isWristInPosition(){
+        return io.isWristInPosition;
+      }
 }
